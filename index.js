4ad75c991cdbd950a07891e354b7475eb66f2939
Raiyan109 <kraiyan109@gmail.com>
2024-01-24 20:35:54 +0600

    cors problem

--- index.js
+++ index.js
@@ -2,13 +2,18 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors');
 require('dotenv').config()
-app.use(cors())
 const userRoutes = require('./routes/user.js');
 const houseRoutes = require('./routes/house.js')
 const bookingRoutes = require('./routes/booking.js')
 const app = express()
 
 app.use(express.json())
+const corsOrigin = {
+    origin: 'http://localhost:5173', //or whatever port your frontend is using
+    credentials: true,
+    optionSuccessStatus: 200
+}
+app.use(cors(corsOrigin));
 
 
 app.use('/api/v1/users', userRoutes)
